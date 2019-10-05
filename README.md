eda22ce130dc46479e6b1211a05e36ca500028f2
ninjaaung <ninja.aung02@gmail.com>
2019-10-04 17:40:06 -0700

    [v0.2-alpha]Most Basic Version of NERE

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Git-New-Repository-Shell
 Shell command that enables a user to create new git repository in the command line | | Terminal
+
+# NERE -- New Repository Shell Command
+Using github api create new repositories  straight from the shell using nere 
+
+## Name
+nere -- new repository
+
+## Synopsis
+nere [-h help] [-u username] [-p personalToken] [-r reponame] [-d description]
+
+## Description
+the nere utility creates a repository on the github user (https://github.com) and remote connect to it via the terminal instantly
+
+    A list of options as follows:
+
+    -h will print the help document for the command
+    -u takes username input from the user
+    -t private token from user's github
+    -r takes and creates the input from the user
+    -d takes and create the description for the repository
+    -p makes the repo private 
+
+
+## Usage
+
+1. Change the file mode to a executible:
+    ```
+    ~ chmod 755 nere.sh
+    ```
+2. Excute with parameter
+    ```
+    ~ ./nere.sh
+    ```
+## Implementing into your path
+in v2.0 checking if shell is stable
---
+++ nere.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+private=false
+description=
+username=
+reponame=
+pat=
+
+usage() # Help and Usage case for the shell
+{
+    echo "usage: nere [-h help] [-u username ] [-p personal_token] [-r reponame] [-d description] "
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -h | --help | help)     shift
+                                usage
+                                ;;
+        -u | --username )       shift
+                                username=$1
+                                ;;
+        -r | --reponame )       shift
+                                reponame=$1
+                                ;;
+        -d | --description )    shift
+                                description=$1
+                                ;;
+        -t | --Token )          shift   
+                                pat=$1
+                                ;;
+        -p | --private )        shift
+                                private=true
+    esac
+    shift
+done
+
+if [ "$pat" != "" ]; then
+    username="$username:$pat"
+    echo $username
+fi
+
+
+if [ -z $username ]; then  
+        read -p "[Username]: " username
+fi
+
+if [ -z $reponame ]; then 
+    read -p "[Reponame]: " reponame 
+fi
+
+curl -s -u $username https://api.github.com/user/repos -d "{\"name\":\"$reponame\", \"private\":\"$private\"}"
