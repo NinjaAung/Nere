3040a7960324fd485408a85fa5b9c1c6c0cee276
ninjaaung <ninja.aung02@gmail.com>
2019-10-17 18:51:59 -0700

    [Added] Comments and Improved PAT and

--- nere.sh
+++ nere.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-private=
-description=
-username=
-reponame=
-pat=
+private=        # Private defaulted to false
+description=    # Optional Description flag    
+username=       # Void Username may move to global
+reponame=       # Void reponame
+pat=            # Personal Access Token or Password may move to globale
+
+
+# Color List
+RED='\x1b[38;5;9m' # Error
+GREEN='\x1B[32m' # Success
+NC='\x1B[37m' # Return Default
+
+
 
 usage() # Help and Usage case for the shell
 {
@@ -25,10 +33,10 @@ while [ "$1" != "" ]; do
         -d | --description )    shift
                                 description=$1
                                 ;;
-     -a | --auththorization )   shift
+        -a | --auth )           shift
                                 pat=$1
                                 ;;
-    -p | --private )            shift
+        -p | --private )        shift
                                 private=true
                                 continue
                                 shift
@@ -36,18 +44,25 @@ while [ "$1" != "" ]; do
     shift
 done
 
-if [ "$pat" != "" ]; then
+
+# if pat 
+if ! [ -z $pat ]; then
     username="$username:$pat"
     echo $username
 fi
 
 
-if [ -z $username ]; then  
+# Checks if $username has been assign a string yet
+if [ -z $username ]; then 
         read -p "[Username]: " username
 fi
 
+
+# Checks if $reponame has been assign a string yet
 if [ -z $reponame ]; then 
     read -p "[Reponame]: " reponame 
 fi
 
+
+# Remove echo for public push v1.0
 echo curl -s -u $username https://api.github.com/user/repos -d "{\"name\":\"$reponame\", \"private\":$private}"
